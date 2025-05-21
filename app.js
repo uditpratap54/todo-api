a82189cb4c3b255a469a46c196d88b14de8c7d8b
uditpratap54 <uditpratapsingh916@gmail.com>
2025-05-21 15:39:19 +0530

    port update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-const port = 5000;
+const port = 5000 ;
 
 app.listen(port, () => {
     console.log(`Server is running at port 5000`);
+    console.log("hello world")
     });
